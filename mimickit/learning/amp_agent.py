mixed precision fix

mimickit/learning/amp_agent.py
@@ -147,12 +147,15 @@ class AMPAgent(ppo_agent.PPOAgent):
 
     def _update_disc(self, batch_size, steps):
         info = dict()
+        device_type = torch.device(self._device).type
 
         for i in range(steps):
             batch = self._exp_buffer.sample(batch_size)
-            with torch.amp.autocast(device_type="cuda", enabled=self._use_mixed_precision, dtype=torch.bfloat16):
+
+            with torch.amp.autocast(device_type=device_type, enabled=self._use_mixed_precision, dtype=torch.bfloat16):
                 loss_info = self._compute_disc_loss(batch)
                 loss = loss_info["disc_loss"]
+
             self._disc_optimizer.step(loss)
 
             torch_util.add_torch_dict(loss_info, info)

mimickit/learning/ase_agent.py
@@ -252,12 +252,15 @@ class ASEAgent(amp_agent.AMPAgent):
 
     def _update_enc(self, batch_size, steps):
         info = dict()
+        device_type = torch.device(self._device).type
 
         for i in range(steps):
             batch = self._exp_buffer.sample(batch_size)
-            with torch.amp.autocast(device_type="cuda", enabled=self._use_mixed_precision, dtype=torch.bfloat16):
+
+            with torch.amp.autocast(device_type=device_type, enabled=self._use_mixed_precision, dtype=torch.bfloat16):
                 loss_info = self._compute_enc_loss(batch)
                 loss = loss_info["enc_loss"]
+
             self._enc_optimizer.step(loss)
 
             torch_util.add_torch_dict(loss_info, info)

mimickit/learning/awr_agent.py
@@ -186,12 +186,15 @@ class AWRAgent(base_agent.BaseAgent):
     
     def _update_critic(self, batch_size, steps):
         info = dict()
+        device_type = torch.device(self._device).type
 
         for i in range(steps):
             batch = self._exp_buffer.sample(batch_size)
-            with torch.amp.autocast(device_type="cuda", enabled=self._use_mixed_precision, dtype=torch.bfloat16):
+
+            with torch.amp.autocast(device_type=device_type, enabled=self._use_mixed_precision, dtype=torch.bfloat16):
                 loss_info = self._compute_critic_loss(batch)
                 loss = loss_info["critic_loss"]
+
             self._critic_optimizer.step(loss)
 
             torch_util.add_torch_dict(loss_info, info)
@@ -201,12 +204,15 @@ class AWRAgent(base_agent.BaseAgent):
 
     def _update_actor(self, batch_size, num_steps):
         info = dict()
+        device_type = torch.device(self._device).type
 
         for i in range(num_steps):
             batch = self._exp_buffer.sample(batch_size)
-            with torch.amp.autocast(device_type="cuda", enabled=self._use_mixed_precision, dtype=torch.bfloat16):
+
+            with torch.amp.autocast(device_type=device_type, enabled=self._use_mixed_precision, dtype=torch.bfloat16):
                 loss_info = self._compute_actor_loss(batch)
                 loss = loss_info["actor_loss"]
+
             self._actor_optimizer.step(loss)
 
             torch_util.add_torch_dict(loss_info, info)